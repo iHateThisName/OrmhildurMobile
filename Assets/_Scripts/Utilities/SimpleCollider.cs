ea1f79f3073b470cea01cf11b1c8b003dd947676
iHateThisName <ivarravi0@gmail.com>
2026-07-02 12:03:46 +0000

    Refactor, Making use of the tag dropdown

--- Assets/_Scripts/Utilities/SimpleCollider.cs
+++ Assets/_Scripts/Utilities/SimpleCollider.cs
@@ -1,10 +1,11 @@
+using Gaskellgames;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class SimpleCollider : MonoBehaviour {
-    [SerializeField] private List<string> tags;
+    [SerializeField, TagDropdown] private List<string> tags;
     //[SerializeField] private LayerMask allowedLayers;
 
     [SerializeField] private UnityEvent<string> OnTriggerEnterEvent;
